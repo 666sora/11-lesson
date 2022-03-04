0afe5630ce34530bf982b9528832f3002de01661
666sora <pavelbys03@gmail.com>
2022-03-04 21:19:41 +0700

    encrypt and decrypt added and choice from them

--- 1-task.cpp
+++ 1-task.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+std::string encrypt_caesar(int n, std::string s) {
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            if (s[i] + n < 'A') {
+                s[i] = ('Z' - ('A' - (s[i] + n))) + 1;
+            }
+            else if (s[i] + n > 'Z') {
+                s[i] = ('A' + ((s[i] + n) - 'Z')) - 1;
+            }
+            else {
+                s[i] += n;
+            }
+        }
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            if (s[i] + n < 'a') {
+                s[i] = ('z' - ('a' - (s[i] + n))) + 1;
+            }
+            else if (s[i] + n > 'z') {
+                s[i] = ('a' + ((s[i] + n) - 'z')) - 1;
+            }
+            else {
+                s[i] += n;
+            }
+        }
+    }
+    return s;
+}
+
+std::string decrypt_caesar (int n, std::string s) {
+    n = -n;
+    return encrypt_caesar(n, s);
+}
+
 int main() {
     int n;
     std::string s;
@@ -12,7 +45,7 @@ int main() {
             correct = false;
             // std::cin.clear();
             // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Wrong shift input!" << std::endl;          
+            std::cout << "Wrong shift input!" << std::endl;
         }
     } while (!correct);
     do {
@@ -25,6 +58,22 @@ int main() {
                 correct = false;
                 std::cout << "Wrong string input!" << std::endl;
             }
+        }
+    } while (!correct);
+    do {
+        correct = true;
+        char answer;
+        std::cout << "Choice from encrypt or decrypt(e/d)";
+        std::cin >> answer;
+        if (answer == 'e') {
+            std::cout << encrypt_caesar(n, s);
+        }
+        else if (answer == 'd') {
+            std::cout << decrypt_caesar(n, s);
+        }
+        else {
+            std::cout << "Error answer input!" << std::endl;
+            correct = false;
         }
     } while (!correct);
     return 0;
